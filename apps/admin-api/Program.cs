73ef6b3e1bbeffff19c46561698123f5c06ac102
LHAR <devlana@outlook.com>
2023-03-19 13:09:28 +0000

    feat: initial expenses end points

--- apps/admin-api/Program.cs
+++ apps/admin-api/Program.cs
@@ -1,6 +1,75 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/ping", () => Results.NoContent());
+
+var expenses = new List<ExpenseDto>
+{
+  new ExpenseDto(
+    1,
+    new Money(10),
+    "EE",
+    "Subscriptions"
+  ),
+  new ExpenseDto(
+    2,
+    new Money(24.99),
+    "The Gym Group",
+    "Subscriptions"
+  ),
+  new ExpenseDto(
+    3,
+    new Money(21.39),
+    "Lidl",
+    "Groceries"
+  ),
+  new ExpenseDto(
+    4,
+    new Money(6),
+    "Blue Star Bus",
+    "Transportation"
+  ),
+  new ExpenseDto(
+    5,
+    new Money(17.98),
+    "Domino's Pizza",
+    "Food"
+  ),
+  new ExpenseDto(
+    6,
+    new Money(30.38),
+    "Wise",
+    "Miscellaneous"
+  ),
+  new ExpenseDto(
+    7,
+    new Money(10),
+    "Education",
+    "Courses"
+  ),
+  new ExpenseDto(
+    8,
+    new Money(695),
+    "House Owner",
+    "Housing"
+  ),
+};
+app.MapGet("api/expenses", () => expenses).WithName("GetExpenses");
+app.MapGet("api/expenses/{id}", (int id) =>
+{
+  var expense = expenses.FirstOrDefault(s => s.id == id);
+  return expense is null ?
+    Results.NotFound() :
+    Results.Ok(expense);
+}).WithName("GetOneExpense");
 
 app.Run();
+
+internal record Money(double Amount, string Currency = "GBP")
+{
+
+}
+internal record ExpenseDto(long id, Money TotalPrice, string Merchant, string Category)
+{
+
+}
