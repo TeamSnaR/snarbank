24fed58c6953f61fefa5cef6ab89bdfc50ec208a
LHAR <devlana@outlook.com>
2023-04-16 12:12:10 +0100

    feat: added cors origin config from app settings
    regen swagger

--- apps/admin-api/Program.cs
+++ apps/admin-api/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
   options.AddPolicy(name: AllowLocal,
     policy =>
     {
-      policy.WithOrigins("http://localhost:4200")
+      policy.WithOrigins(builder.Configuration["AllowedHosts"] ?? "localhost")
         .AllowAnyHeader()
         .AllowAnyMethod();
     });
--- libs/generated/admin-api-types/src/index.ts
+++ libs/generated/admin-api-types/src/index.ts
@@ -1,3 +1,3 @@
 export * from './interfaces/expense-dto';
-export * from './interfaces/money';
 export * from './interfaces/expense';
+export * from './interfaces/money';
